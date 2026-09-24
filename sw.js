47545d2582e5a5f2ab30ac7734d53613c98559b6
estevaoooooooo <estevaogimeneztesch@gmail.com>
2026-09-24 10:09:46 -0300

    Add ZRV Lap Timer PWA: sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE='zrv-lap-timer-v1';
+const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','assets/icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(response=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin){const copy=response.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return response}).catch(()=>caches.match('./')))));
+
