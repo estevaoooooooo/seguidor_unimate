71edfb915296130328ab16a1029fe224f0c01155
estevaoooooooo <estevaogimeneztesch@gmail.com>
2026-09-24 10:32:57 -0300

    Bust PWA cache for release v2: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='zrv-lap-timer-v2';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','assets/icon.svg'];
+const ASSETS=['./?v=2','index.html','styles.css?v=2','app.js?v=2','manifest.webmanifest?v=2','assets/icon.svg?v=2'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(response=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin){const copy=response.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return response}).catch(()=>caches.match('./')))));
